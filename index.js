33cb36f4167d16018bab8cb36907c66bf905c4b1
Keyifciyiz FM <keyifciyizfmm@gmail.com>
2026-05-04 00:12:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,24 +13,6 @@ let users = {};
 const masterNick = "Keyifciyiz_Fm"; 
 const masterPass = "123456";
 
-function parseEmojis(text) {
-    const emojiMap = {
-        ":smile:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f60a.svg",
-        ":joy:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f602.svg",
-        ":heart:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/2764.svg",
-        ":fire:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f525.svg",
-        ":microphone:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f399.svg",
-        ":cool:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f60e.svg",
-        ":thumbsup:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f44d.svg",
-        ":rose:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f339.svg"
-    };
-    let newText = text;
-    for (const [code, url] of Object.entries(emojiMap)) {
-        newText = newText.replace(new RegExp(code, 'g'), `<img src="${url}" style="width:20px; height:20px; vertical-align:middle; margin:0 2px;">`);
-    }
-    return newText;
-}
-
 io.on('connection', (socket) => {
     socket.on('join', (data) => {
         if (data.nick === masterNick && data.password !== masterPass) {
@@ -42,27 +24,21 @@ io.on('connection', (socket) => {
         users[socket.id] = { nick: socket.nick, color: socket.color, role: socket.role };
         socket.emit('login success', { role: socket.role });
         io.emit('user list', Object.values(users));
-        io.emit('chat message', { user: 'SİSTEM', text: `${socket.nick} bağlandı!`, system: true });
     });
 
-    // Ses Verisi Aktarımı
+    // SES PAKETİ AKTARIMI
     socket.on('audio-stream', (data) => {
         socket.broadcast.emit('audio-receive', data);
     });
 
     socket.on('chat message', (data) => {
-        io.emit('chat message', { user: socket.nick, text: parseEmojis(data.text), color: socket.color, style: data.style });
+        io.emit('chat message', { user: socket.nick, text: data.text, color: socket.color, style: data.style });
     });
 
     socket.on('disconnect', () => {
-        if (users[socket.id]) {
-            const nick = users[socket.id].nick;
-            delete users[socket.id];
-            io.emit('user list', Object.values(users));
-            io.emit('chat message', { user: 'SİSTEM', text: `${nick} ayrıldı.`, system: true });
-        }
+        delete users[socket.id];
+        io.emit('user list', Object.values(users));
     });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Radyo Yayında!`));
+server.listen(process.env.PORT || 3000);
