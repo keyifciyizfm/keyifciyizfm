3cf97fcb013249aac2041ad83fb0bf7b423880db
Keyifciyiz FM <keyifciyizfmm@gmail.com>
2026-05-03 21:00:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-// index.js içindeki parseEmojis fonksiyonunu bununla değiştir:
-function parseEmojis(text) {
-    const emojiMap = {
-        ":smile:": "😊",
-        ":joy:": "😂",
-        ":heart:": "❤️",
-        ":fire:": "🔥",
-        ":microphone:": "🎙️",
-        ":cool:": "😎",
-        ":thumbsup:": "👍",
-        ":rose:": "🌹"
-    };
-
-    let newText = text;
-    for (const [code, emoji] of Object.entries(emojiMap)) {
-        newText = newText.replace(new RegExp(code, 'g'), emoji);
-    }
-    return newText;
-}
+// index.js içindeki chat message kısmını bu hale getir
+socket.on('chat message', (data) => {
+    // Karmaşık çevirme işlemlerini bırakıyoruz, gelen mesajı olduğu gibi iletiyoruz
+    io.emit('chat message', { 
+        user: socket.nick, 
+        text: data.text, 
+        color: socket.color, 
+        style: data.style 
+    });
+});
