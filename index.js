c01cb2e874e490a32d7161c8f55392daf10e7ede
Keyifciyiz FM <keyifciyizfmm@gmail.com>
2026-05-04 02:10:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,41 +12,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 let users = {}; 
 let bannedIPs = [];
 
-// YÖNETİCİ BİLGİLERİ
 const masterNick = "Keyifciyiz_Fm";
 const masterPass = "123456";
 
 function parseEmojis(text) {
     const emojiMap = {
-        ":smile:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f60a.svg",
-        ":joy:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f602.svg",
-        ":heart:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/2764.svg",
-        ":fire:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f525.svg",
-        ":microphone:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f399.svg",
-        ":cool:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f60e.svg",
-        ":thumbsup:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f44d.svg",
-        ":rose:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f339.svg"
+        ":smile:": "1f60a", ":joy:": "1f602", ":cool:": "1f60e", ":heart:": "2764",
+        ":fire:": "1f525", ":rose:": "1f339", ":thumbsup:": "1f44d", ":microphone:": "1f399",
+        ":wink:": "1f609", ":star:": "2b50", ":coffee:": "2615", ":musical_note:": "1f3b5"
     };
     let newText = text;
-    for (const [code, url] of Object.entries(emojiMap)) {
-        newText = newText.replace(new RegExp(code, 'g'), `<img src="${url}" style="width:20px; height:20px; vertical-align:middle; margin:0 2px;">`);
+    for (const [code, id] of Object.entries(emojiMap)) {
+        const url = `https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/${id}.svg`;
+        newText = newText.replace(new RegExp(code, 'g'), `<img src="${url}" style="width:22px; height:22px; vertical-align:middle; margin:0 2px;">`);
     }
     return newText;
 }
 
 io.on('connection', (socket) => {
     const userIP = socket.handshake.address;
-
     socket.on('join', (data) => {
         if (bannedIPs.includes(userIP)) return socket.emit('auth error', 'Girişiniz engellendi!');
         if (data.nick === masterNick && data.password !== masterPass) return socket.emit('auth error', 'Hatalı Şifre!');
-
         socket.nick = data.nick || "Misafir";
         socket.role = (data.nick === masterNick) ? 'Yönetici' : 'Dinleyici';
         socket.color = (socket.role === 'Yönetici') ? '#ff4757' : '#2ecc71';
-        
         users[socket.id] = { id: socket.id, nick: socket.nick, role: socket.role, color: socket.color, ip: userIP };
-        
         socket.emit('login success', { role: socket.role, nick: socket.nick });
         io.emit('user list', Object.values(users));
     });
@@ -55,7 +46,6 @@ io.on('connection', (socket) => {
         if (socket.role !== 'Yönetici') return;
         const targetId = Object.keys(users).find(id => users[id].nick === data.targetNick);
         if (!targetId) return;
-
         if (data.action === 'kick') {
             io.to(targetId).emit('force logout', 'Odadan atıldınız!');
             io.sockets.sockets.get(targetId)?.disconnect();
@@ -69,10 +59,9 @@ io.on('connection', (socket) => {
     socket.on('chat message', (data) => {
         if (users[socket.id]) {
             const u = users[socket.id];
-            const cleanText = parseEmojis(data.text);
             io.emit('chat message', { 
                 user: u.nick, 
-                text: cleanText, 
+                text: parseEmojis(data.text), 
                 color: data.color || u.color, 
                 style: data.style 
             });
@@ -80,10 +69,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        if (users[socket.id]) {
-            delete users[socket.id];
-            io.emit('user list', Object.values(users));
-        }
+        if (users[socket.id]) { delete users[socket.id]; io.emit('user list', Object.values(users)); }
     });
 });
 
