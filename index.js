827fbcb2841934cc811c5b494632f57756439e7b
Keyifciyiz FM <keyifciyizfmm@gmail.com>
2026-05-04 00:33:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,32 +10,61 @@ const io = new Server(server);
 app.use(express.static(path.join(__dirname, 'public')));
 
 let users = {}; 
+
+// YÖNETİCİ AYARLARI
 const masterNick = "Keyifciyiz_Fm"; 
 const masterPass = "123456";
 
+function parseEmojis(text) {
+    const emojiMap = {
+        ":smile:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f60a.svg",
+        ":joy:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f602.svg",
+        ":heart:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/2764.svg",
+        ":fire:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f525.svg",
+        ":microphone:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f399.svg",
+        ":cool:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f60e.svg",
+        ":thumbsup:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f44d.svg",
+        ":rose:": "https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/1f339.svg"
+    };
+    let newText = text;
+    for (const [code, url] of Object.entries(emojiMap)) {
+        newText = newText.replace(new RegExp(code, 'g'), `<img src="${url}" style="width:20px; height:20px; vertical-align:middle; margin:0 2px;">`);
+    }
+    return newText;
+}
+
 io.on('connection', (socket) => {
     socket.on('join', (data) => {
+        // Şifre Kontrolü
         if (data.nick === masterNick && data.password !== masterPass) {
             return socket.emit('auth error', 'Hatalı Şifre!');
         }
+
         socket.nick = data.nick || "Misafir";
+        // Yöneticiye DJ rolü ve sarı renk veriyoruz
         socket.role = (data.nick === masterNick) ? 'DJ' : 'Dinleyici';
         socket.color = (socket.role === 'DJ') ? '#f1c40f' : (data.color || "#2ecc71");
+        
         users[socket.id] = { nick: socket.nick, color: socket.color, role: socket.role };
         
-        socket.emit('login success', { role: socket.role, nick: socket.nick });
+        socket.emit('login success');
         io.emit('user list', Object.values(users));
+        io.emit('chat message', { user: 'SİSTEM', text: `${socket.nick} bağlandı!`, system: true });
     });
 
-    // DJ'den gelen ses paketini yakala ve herkese (broadcast) gönder
-    socket.on('audio-stream', (data) => {
-        socket.broadcast.emit('audio-receive', data);
+    socket.on('change color', (newColor) => {
+        if (users[socket.id]) {
+            users[socket.id].color = newColor;
+            socket.color = newColor;
+            io.emit('user list', Object.values(users));
+        }
     });
 
     socket.on('chat message', (data) => {
+        const cleanText = parseEmojis(data.text); 
         io.emit('chat message', { 
             user: socket.nick, 
-            text: data.text, 
+            text: cleanText, 
             color: socket.color, 
             style: data.style 
         });
@@ -43,8 +72,10 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         if (users[socket.id]) {
+            const nick = users[socket.id].nick;
             delete users[socket.id];
             io.emit('user list', Object.values(users));
+            io.emit('chat message', { user: 'SİSTEM', text: `${nick} ayrıldı.`, system: true });
         }
     });
 });
