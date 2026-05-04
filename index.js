f5277103a0b0cf535e6372d51a67cfc6ddaefe73
Keyifciyiz FM <keyifciyizfmm@gmail.com>
2026-05-05 02:57:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    maxHttpBufferSize: 1e7 // Ses verisi için tampon bellek boyutunu artırdık
+    maxHttpBufferSize: 1e8, // 100MB Ses verisi kapasitesi
+    cors: { origin: "*" }
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,26 +17,11 @@ let bannedIPs = [];
 const masterNick = "Keyifciyiz_Fm";
 const masterPass = "123456";
 
-// Emoji Çözücü
-const emojiMap = {
-    ":smile:": "1f60a", ":joy:": "1f602", ":cool:": "1f60e", ":heart:": "2764",
-    ":fire:": "1f525", ":rose:": "1f339", ":thumbsup:": "1f44d", ":microphone:": "1f399"
-};
-
-function parseEmojis(text) {
-    let newText = text;
-    for (const [code, id] of Object.entries(emojiMap)) {
-        const url = `https://raw.githubusercontent.com/jakejarvis/apple-emoji-svg/main/emoji/${id}.svg`;
-        newText = newText.split(code).join(`<img src="${url}" style="width:20px; vertical-align:middle;">`);
-    }
-    return newText;
-}
-
 io.on('connection', (socket) => {
     const userIP = socket.handshake.headers['x-forwarded-for'] || socket.handshake.address;
 
     socket.on('join', (data) => {
-        if (bannedIPs.includes(userIP)) return socket.emit('auth error', 'Girişiniz yasaklandı!');
+        if (bannedIPs.includes(userIP)) return socket.emit('auth error', 'Girişiniz yasaklıdır!');
         if (data.nick === masterNick && data.password !== masterPass) return socket.emit('auth error', 'Hatalı DJ şifresi!');
 
         socket.nick = data.nick || "Misafir_" + Math.floor(Math.random() * 999);
@@ -47,7 +33,7 @@ io.on('connection', (socket) => {
         io.emit('user list', Object.values(users));
     });
 
-    // CANLI SES İLETİMİ (DJ'den Dinleyicilere)
+    // SES AKTARIMI: DJ'den gelen veriyi herkese yay
     socket.on('audio-data', (data) => {
         if (socket.role === 'Yönetici') {
             socket.broadcast.emit('audio-stream', data);
@@ -58,9 +44,8 @@ io.on('connection', (socket) => {
         if (users[socket.id]) {
             io.emit('chat message', {
                 user: users[socket.id].nick,
-                text: parseEmojis(data.text),
-                color: data.color || users[socket.id].color,
-                style: data.style
+                text: data.text,
+                color: data.color || users[socket.id].color
             });
         }
     });
@@ -70,7 +55,7 @@ io.on('connection', (socket) => {
         const targetId = Object.keys(users).find(id => users[id].nick === data.targetNick);
         if (targetId) {
             if (data.action === 'kick') {
-                io.to(targetId).emit('force logout', 'Atıldınız!');
+                io.to(targetId).emit('force logout', 'DJ tarafından odadan atıldınız!');
                 io.sockets.sockets.get(targetId)?.disconnect();
             } else if (data.action === 'ban') {
                 bannedIPs.push(users[targetId].ip);
@@ -85,4 +70,5 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => console.log('Sohbet ve Radyo 3000 portunda hazır!'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Sohbet ve Ses Yayını ${PORT} portunda aktif.`));
